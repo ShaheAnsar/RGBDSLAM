ef08ab0a0a6a24facee412b10f25be99f0dd00de
M. Shahe Ansar <shaheansar@protonmail.com>
2022-10-28 00:55:21 +0530

    Refactor freenect2 implementation into kinect_preprocess.py

--- kinect_preprocess.py
+++ kinect_preprocess.py
@@ -1,49 +1,21 @@
-#Data was gathered from: https://www.researchgate.net/publication/321048476_A_Post-Rectification_Approach_of_Depth_Images_of_Kinect_v2_for_3D_Reconstruction_of_Indoor_Scenes
-
 import numpy as np
+import cv2
+from freenect2 import Device, FrameType
 
-#In mm
-FxIR = 388.198
-FyIR = 389.033
 
-#In Pixel Coordinates
-DEPTH_WIDTH = 512
-DEPTH_HEIGHT = 424
-CxIR = DEPTH_WIDTH//2
-CyIR = DEPTH_HEIGHT//2
-
-#Distortion Coefficients
-K1_IR = 0.126
-K2_IR = -0.329
-K3_IR = 0.111
-P1_IR = -0.001
-P2_IR = -0.002
-
-#Skew
-S_IR = 0
-
-#IR_DEPTHSHIFT = 60.358
-IR_DEPTHSHIFT = 110.0
-
-RMat = np.array([[0.99997, 0.00715, -0.00105],
-    [-0.00715, 0.99995, 0.00662],
-    [0.00110, -0.00661, 0.99998]])
-Tvec = np.array([-0.06015, 0.00221, 0.02714])
-
-#KinvMat = np.array([[1/FxIR, 
-
-def convert_from_uvd(u, v, d):
-    d -= IR_DEPTHSHIFT
-    x_over_z = (CxIR - u)/FxIR
-    y_over_z = (CyIR - v)/FyIR
-    z = d / (1 + x_over_z**2 + y_over_z**2)
-    x_uncorr = x_over_z 
-    y_uncorr = y_over_z
-    r2 = x_uncorr**2 + y_uncorr**2
-    #x = x_uncorr
-    #y = y_uncorr
-    x = x_uncorr * (1 + K1_IR*r2 + K2_IR*r2**2 + K3_IR*r2**3) + 2*P1_IR*x_uncorr*y_uncorr + P2_IR*(r2 + 2*x_uncorr**2)
-    x *= z
-    y = y_uncorr * (1 + K1_IR*r2 + K2_IR*r2**2 + K3_IR*r2**3) + 2*P2_IR*x_uncorr*y_uncorr + P1_IR*(r2 + 2*y_uncorr**2)
-    y *= z
-    return x, y, z
+def convert_rgb_depth_to_pcl(device, rgb_frame, depth_frame):
+    depth_undistorted, rgb_registered = device.registration.apply(rgb_frame, depth_frame, enable_filter=True)
+    point_cloud = device.registration.get_points_xyz_array(depth_undistorted)
+    indices = ~np.isnan(point_cloud)
+    point_cloud = point_cloud[indices]
+    rgb_cloud = rgb_registered.to_array()
+    cv2.imshow("RGB Reg", rgb_cloud)
+    cv2.waitKey(0)
+    rgb_cloud = rgb_cloud[:,:,:3]
+    rgb_cloud = cv2.cvtColor(rgb_cloud, cv2.COLOR_BGR2RGB)
+    rgb_cloud = rgb_cloud[indices]
+    s = point_cloud.shape
+    point_cloud = point_cloud.reshape((s[0]//3, 3))
+    s = rgb_cloud.shape
+    rgb_cloud = rgb_cloud.reshape((s[0]//3, 3))
+    return (point_cloud, rgb_cloud)
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#from freenect2 import Device, FrameType
+from freenect2 import Device, FrameType
 import numpy as np
 import cv2
 import vedo
@@ -25,37 +25,6 @@ with device.running():
             rgb_frame = frame
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-#except as e:
-#    print("Issue encountered")
-#    print(e)
-#    exit(-1)
-
-#cv2.destroyAllWindows()
-depth_undistorted, rgb_registered = device.registration.apply(rgb_frame, depth_frame, enable_filter=False)
-point_cloud = device.registration.get_points_xyz_array(depth_undistorted)
-indices = ~np.isnan(point_cloud)
-point_cloud = point_cloud[indices]
-rgb_cloud = rgb_registered.to_array()
-cv2.imshow("RGB Reg", rgb_cloud)
-cv2.waitKey(0)
-rgb_cloud = rgb_cloud[:,:,:3]
-rgb_cloud = cv2.cvtColor(rgb_cloud, cv2.COLOR_BGR2RGB)
-rgb_cloud = rgb_cloud[indices]
-s = point_cloud.shape
-point_cloud = point_cloud.reshape((s[0]//3, 3))
-s = rgb_cloud.shape
-rgb_cloud = rgb_cloud.reshape((s[0]//3, 3))
->>>>>>> main
-print(point_cloud.shape)
-print(point_cloud)
+point_cloud, rgb_cloud = convert_rgb_depth_to_pcl(device, rgb_frame, depth_frame)
 pts = vedo.Points(point_cloud, c=rgb_cloud)
 vedo.show(pts, __doc__, axes=True)
-<<<<<<< HEAD
-print("Ended")
-m.cleanup()
-#fig = plt.figure(figsize=(8,8))
-#ax = fig.add_subplot(111, projection="3d")
-#ax.scatter(x, y, z)
-#plt.show()
-=======
->>>>>>> main
