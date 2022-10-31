44e5d9c04cc6af641289da9bb935d217bf8f0951
M. Shahe Ansar <shaheansar@protonmail.com>
2022-10-31 16:59:48 +0530

    Added function to interpolate the depth between pixels

--- kinect_preprocess.py
+++ kinect_preprocess.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math as m
 import cv2
 from freenect2 import Device, FrameType
 
@@ -19,3 +20,24 @@ def convert_rgb_depth_to_pcl(device, rgb_frame, depth_frame):
     s = rgb_cloud.shape
     rgb_cloud = rgb_cloud.reshape((s[0]//3, 3))
     return (point_cloud, rgb_cloud)
+
+def get_depth(depth_undistorted, x, y):
+    x_l = m.floor(x)
+    y_l = m.floor(y)
+    x_u = m.ceil(x)
+    y_u = m.ceil(y)
+    # ul - upper left, lr - lower right
+    # ll - lower left, ur - upper right
+    depth_ul = depth_undistorted[y_l, x_l]
+    depth_lr = depth_undistorted[y_u, x_u]
+    depth_ur = depth_undistorted[y_l, x_u]
+    depth_ll = depth_undistorted[y_u, x_l]
+    # Interpolation const in the y dir
+    t_y = y - y_l
+    # Interpolation const in the x dir
+    t_x = x - x_l
+    depth_y = t_y*depth_ll + (1 - t_y)*depth_ul
+    depth_x = t_x*depth_lr + (1-t_x)*depth_ll
+    return 0.5*depth_x + 0.5*depth_y
+
+
