e410cfd111f4edd2ef4c4fdb937b6f84ed64c583
M. Shahe Ansar <shaheansar@protonmail.com>
2022-10-31 11:20:50 +0530

    Added frame saving

--- save_frame.py
+++ save_frame.py
@@ -33,7 +33,14 @@ with device.running():
             rgb_frame = frame
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-cv2.imwrite(path.join(ASSET_DIR, sys.argv[1] + "_depth.png"), depth_undistorted)
-point_cloud, rgb_cloud = convert_rgb_depth_to_pcl(device, rgb_frame, depth_frame)
-pts = vedo.Points(point_cloud, c=rgb_cloud)
-vedo.show(pts, __doc__, axes=True)
+depth_undistorted, rgb_registered = device.registration.apply(rgb_frame, depth_frame, enable_filter=True)
+depth_undistorted = depth_undistorted.to_array()
+print(np.max(depth_undistorted))
+rgb_registered = rgb_registered.to_array()
+cv2.imwrite(path.join(ASSET_DIR, sys.argv[1] + "_rgb.png"), rgb_registered)
+with open(path.join(ASSET_DIR, sys.argv[1] + "_depth.npy"), "wb") as f:
+    np.save(f, depth_undistorted)
+#cv2.imwrite(path.join(ASSET_DIR, sys.argv[1] + "_depth.png"), depth_undistorted)
+#point_cloud, rgb_cloud = convert_rgb_depth_to_pcl(device, rgb_frame, depth_frame)
+#pts = vedo.Points(point_cloud, c=rgb_cloud)
+#vedo.show(pts, __doc__, axes=True)
